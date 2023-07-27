1787ea282c6ba33220b0f4805058203edf8918db
jpg22 <jpg22@tamu.edu>
2023-07-27 14:55:48 -0500

    04qualifiers

--- src/main/java/com/luv2code/springcoredemo/rest/DemoController.java
+++ src/main/java/com/luv2code/springcoredemo/rest/DemoController.java
@@ -2,6 +2,7 @@ package com.luv2code.springcoredemo.rest;
 
 import com.luv2code.springcoredemo.common.Coach;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,8 +14,8 @@ public class DemoController {
     private Coach myCoach;
 
     @Autowired
-    public void setCoach(Coach theCoach){
-        myCoach = theCoach;
+    public DemoController(@Qualifier("baseballCoach") Coach theCoach){
+        myCoach=theCoach;
     }
 
     @GetMapping("dailyworkout")
