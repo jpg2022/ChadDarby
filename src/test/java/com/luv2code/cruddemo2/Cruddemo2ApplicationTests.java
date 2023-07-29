c9272803ad794705dd30edf442fedbc822e3cfa6
jpg22 <jpg22@tamu.edu>
2023-07-29 15:08:22 -0500

    Init

---
+++ src/test/java/com/luv2code/cruddemo2/Cruddemo2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luv2code.cruddemo2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cruddemo2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
