4fd383a72d2bd04f653b08ee6c3bdd95c34fdd81
jpg22 <jpg22@tamu.edu>
2023-07-29 20:55:29 -0500

    Finished and moving on to jpa

---
+++ src/main/java/com/luv2code/cruddemo2/rest/EmployeeRestController.java
@@ -0,0 +1,60 @@
+package com.luv2code.cruddemo2.rest;
+
+import com.luv2code.cruddemo2.dao.EmployeeDAO;
+import com.luv2code.cruddemo2.dao.EmployeeDAOJpaImpl;
+import com.luv2code.cruddemo2.entity.Employee;
+import com.luv2code.cruddemo2.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class EmployeeRestController {
+    private EmployeeService employeeService;
+    @Autowired
+    public EmployeeRestController(EmployeeService theEmployeeService){
+        employeeService = theEmployeeService;
+    }
+
+    @GetMapping("/employees")
+    public List<Employee> findAll(){
+        return employeeService.findAll();
+    }
+
+    //get employee by id
+    @GetMapping("/employees/{employeeId}")
+    public Employee getEmployee(@PathVariable int employeeId){
+        Employee theEmployee = employeeService.findById(employeeId);
+        if(theEmployee == null){
+            throw new RuntimeException("Employee not found");
+        }
+        return  theEmployee;
+    }
+
+    //post employee to db
+    @PostMapping("/employees")
+    public Employee addEmployee(@RequestBody Employee theEmployee){
+        theEmployee.setId(0);
+        Employee dbEmployee = employeeService.save(theEmployee);
+        return dbEmployee;
+    }
+    @PutMapping("/employees")
+    public Employee updateEmployee(@RequestBody Employee theEmployee){
+        Employee dbEmployee = employeeService.save(theEmployee);
+        return dbEmployee;
+    }
+
+    @DeleteMapping("/employees/{employeeId}")
+    public String deleteEmployee(@PathVariable int employeeId){
+        Employee tempEmployee = employeeService.findById(employeeId);
+
+        if(tempEmployee == null){
+            throw new RuntimeException("Employee id not found");
+        }
+        employeeService.deleteById(employeeId);
+        return "Deleted employee with id " + employeeId;
+    }
+
+}
---
+++ src/main/java/com/luv2code/cruddemo2/service/EmployeeServiceImpl.java
@@ -0,0 +1,39 @@
+package com.luv2code.cruddemo2.service;
+
+import com.luv2code.cruddemo2.dao.EmployeeDAO;
+import com.luv2code.cruddemo2.entity.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+//Service is for custom business logic, and for combining multiple daos
+@Service
+public class EmployeeServiceImpl implements EmployeeService{
+
+    private EmployeeDAO employeeDAO;
+    @Autowired
+    public EmployeeServiceImpl(EmployeeDAO theEmployeeDAO){
+        employeeDAO=theEmployeeDAO;
+    }
+
+    @Override
+    public List<Employee> findAll() {
+        return employeeDAO.findAll();
+    }
+
+    @Override
+    public Employee findById(int theId) {
+        return employeeDAO.findById(theId);
+    }
+    @Transactional
+    @Override
+    public Employee save(Employee theEmployee) {
+        return employeeDAO.save(theEmployee);
+    }
+    @Transactional
+    @Override
+    public void deleteById(int theId) {
+        employeeDAO.deleteById(theId);
+    }
+}
